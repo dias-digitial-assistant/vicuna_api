31ac71b00de816dfeb529328bec2b4bb5472db77
sudarshan-kamath <sudkamath.de@gmail.com>
2023-05-08 12:17:15 +0200

    Last chunk has no empty byte: correction

--- test_langchain_vicuna.py
+++ test_langchain_vicuna.py
@@ -1,5 +1,7 @@
 from vicuna_llm import VicunaLLM
 
 llm = VicunaLLM(server_url="http://localhost:52529")
+llm.pload["temperature"] = 0.8
+llm.pload["max_new_tokens"] = 1500
 
-print(llm("Hello, how are you?",stop=["\n###"]))
+print(llm("Du bist sehr frohliche assistent, der Emojis mit dem Text antworten kann. Hallo, wie geht's?",stop=["\n### Human:"]))
